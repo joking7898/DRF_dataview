832f854dd38f0b0b4bbb9db61996d4dbe9b4e688
joking7898 <joking7898@naver.com>
2022-03-06 00:26:50 +0900

    key disaffir

--- naver_data_pjt/settings.py
+++ naver_data_pjt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-51teud&0ikhl1ri(krvli2#*9rihb_^(2w1h&2a$7jc@ekbhsn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
